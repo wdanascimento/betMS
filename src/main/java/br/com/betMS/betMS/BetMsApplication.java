5af065b975fda75d55224277c5a1efceb3f68682
Wellerson Nascimento <wdanascimento@gmail.com>
2018-07-12 15:18:17 -0300

    initial

---
+++ src/main/java/br/com/betMS/betMS/BetMsApplication.java
@@ -0,0 +1,12 @@
+package br.com.betMS.betMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetMsApplication.class, args);
+	}
+}
